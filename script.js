0e56f1ee551a0cb46ad803ddf6ca7b5316c8165b
Cinn <232077108+cinnammon-n@users.noreply.github.com>
2026-02-14 15:05:43 +0530

    added new gifs and updated script.js

--- script.js
+++ script.js
@@ -9,19 +9,44 @@ secret.addEventListener('mouseout', () => {
 function randomGifGenerator() {
     const gifs = [
         "gifs/elmo.gif",
+        "gifs/catstealtato.gif",
         "gifs/elmotato.gif",
+        "gifs/lifetato2.gif",
         "gifs/tatointensify.gif",
-        "gifs/potatointensify.gif",
+        "gifs/tatoyeet.gif",
         "gifs/tatowiggle.gif",
+        "gifs/lifetato3.gif",
         "gifs/elmobob.gif",
         "gifs/fries.gif",
         "gifs/friesagain.gif",
-        "gifs/elmodance.gif"
+        "gifs/tatoshower.gif",
+        "gifs/elmodance.gif",
+        "gifs/lifetato.gif",
+        "gifs/showmetatosalad.gif",
+        "gifs/tatowar.gif",
+        "gifs/fryman.gif",
+        "gifs/tatogun.gif",
+        "gifs/tatoyeeet.gif",
+        "gifs/sadtato.gif",
+        "gifs/tatosacrifice.gif",
+        "gifs/tatopacman.gif",
+        "gifs/scaredtatoes.gif",
+        "gifs/tatocrackers.gif",
+        "gifs/kirbychips.gif",
+        "gifs/chipsandsoda.gif",
+        "gifs/halftato.gif",
+        "gifs/tatonom.gif",
+        "gifs/tatosacrifice2.gif",
+        "gifs/tatochipinspace.gif",
+        "gifs/tatosaladd.gif",
+        "gifs/yumyumtatosalad.gif",
+        "gifs/potaytoes.gif",
+        "gifs/sandintatosalad.gif"
     ];
     const randomGif = gifs[Math.floor(Math.random() * gifs.length)];
     document.getElementById("randomGif").src = randomGif;
 }
 
 randomGifGenerator();
-setInterval(randomGifGenerator, 3000);
+setInterval(randomGifGenerator, 4000);
 
